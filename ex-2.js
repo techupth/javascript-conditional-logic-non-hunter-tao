850da973b314c8049bafa160ed0841cfafebcd17
Prechak <benedict.precha@gmail.com>
2024-05-15 12:15:14 +0700

    (ex-2): broken light bulb

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,11 @@
 //Exercise 2
 let lightBulbStatus = "On";
-
+lightBulbStatus = "Broken";
 // Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {
+  console.log("Light bulb is Off.");
+}
