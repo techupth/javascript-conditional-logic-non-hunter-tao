f16d8b7adb77dd121287479aa4dbafff6dfb71fc
Prechak <benedict.precha@gmail.com>
2024-05-15 12:30:38 +0700

    (ex-4): switch the light bulb

--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,16 @@
 //Exercise 4
 // Switch Statement
-let lightBulbStatus = "On";
+let lightBulbStatus = "Broken";
 
 //Start coding here
+switch (lightBulbStatus) {
+  case "On":
+    console.log("Light bulb is On.");
+    break;
+  case "Broken":
+    console.log("Light bulb is Broken.");
+    break;
+  case "Off":
+    console.log("Light bulb is Off.");
+    break;
+}
